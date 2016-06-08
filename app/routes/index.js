b9ecc09e348c922f9d2d883e42c0a292cc3d3cd6
Luca Quatela and Patrick Wells <patrick@>
2016-06-08 16:14:41 -0700

    display only the first 5 entries

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('post');
+    // var allPosts = this.store.findAll('post');
+    return this.store.query('post', {limitToLast: 5});
+
   },
   actions: {
     save(newPost) {
