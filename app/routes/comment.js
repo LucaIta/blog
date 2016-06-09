5ddbf187b69cd5f103fc838a71d4f6258fac001c
Luca Quatela and Patrick Wells <luca@>
2016-06-09 15:01:31 -0700

    display in the index the last 5 comments

--- app/routes/comment.js
+++ app/routes/comment.js
@@ -2,11 +2,17 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params){
+
     return this.store.findRecord('comment', params.comment_id);
   },
   actions: {
     update(comment) {
       comment.save();
+    },
+    delete(model) {
+      model.destroyRecord();
+      console.log(model.post);
+      this.transitionTo("index");
     }
   }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,8 +3,10 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     // var allPosts = this.store.findAll('post');
-    return this.store.query('post', {limitToLast: 5});
-
+    return Ember.RSVP.hash({
+      posts: this.store.query('post', {limitToLast: 5}),
+      comments: this.store.query('comment', {limitToLast: 5})
+    });
   },
   actions: {
     save(newPost) {
